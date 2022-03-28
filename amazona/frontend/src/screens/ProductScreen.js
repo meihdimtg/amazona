c5aa27b4428b7213bd05f420f6230793188b57ab
Mehdi Mettag <meihdi.mtg@gmail.com>
2022-03-28 20:17:40 +0200

    Video 14

--- amazona/frontend/src/screens/ProductScreen.js
+++ amazona/frontend/src/screens/ProductScreen.js
@@ -11,6 +11,9 @@ import { useParams } from 'react-router-dom';
 import logger from 'use-reducer-logger';
 import Product from '../components/Product';
 import Rating from '../components/Rating';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
+import { getError } from '../utils';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -41,15 +44,15 @@ function ProductScreen() {
         const result = await axios.get(`/api/products/slug/${slug}`);
         dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
       } catch (err) {
-        dispatch({ type: 'FETCH_FAIL', payload: err.message });
+        dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
       }
     };
     fetchData();
   }, [slug]);
   return loading ? (
-    <div>Loading...</div>
+    <LoadingBox />
   ) : error ? (
-    <div>{error}</div>
+    <MessageBox variant="danger">{error}</MessageBox>
   ) : (
     <div>
       <Row>
