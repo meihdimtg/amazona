8b26a8bf19eda1262f86c7dfeb15dd4d989ce0fc
Mehdi Mettag <meihdi.mtg@gmail.com>
2022-03-28 12:08:53 +0200

    Video 10 finish

--- amazona/frontend/src/screens/HomeScreen.js
+++ amazona/frontend/src/screens/HomeScreen.js
@@ -41,12 +41,7 @@ function HomeScreen() {
     <div>
       <h1>Featured Products</h1>
       <div className="products">
-        {loading ? (
-          <div>loading...</div>
-        ) : error ? (
-          <div>{error}</div>
-        ) : ( 
-        {products.map((product) => (
+        {data.products.map((product) => (
           <div className="product" key={product.slug}>
             <Link to={`/product/${product.slug}`}>
               <img src={product.image} alt={product.name} />
