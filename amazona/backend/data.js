496e05d96fb8c6b6f7f22bf0e7016be4aae56191
Mehdi Mettag <meihdi.mtg@gmail.com>
2022-04-15 18:11:05 +0200

    Video 21 fini

--- amazona/backend/data.js
+++ amazona/backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+  users: [
+    {
+      name: 'Meihdi',
+      email: 'meihdi.mtg@gmail.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'Karl',
+      email: 'karl.mtg@gmail.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       // _id: '1',
